1f493d38d923d473e93211796ad840d306945735
harsha <harshaudayanga401@gmail.com>
2024-04-26 09:54:50 +0530

    headcommit

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head  ><title>Taproscapes</title>
+      <Head  >Taproscapes
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100..900&display=swap" rel="stylesheet"></link>
